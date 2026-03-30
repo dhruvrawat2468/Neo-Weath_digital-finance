31f48e9a85eda426c95eac7e837ae3165c72bcbb
dhruvrawat2468 <rawatdhruv200.dr@gmail.com>
2026-03-30 13:10:07 +0530

    Fixed the home button

--- components/ui/home-button.jsx
+++ components/ui/home-button.jsx
@@ -19,8 +19,8 @@ const HomeButton = () => {
           font-size: 17px;
           border-radius: 500px;
           overflow: hidden;
-          background: #6244e7;
-          color: ghostwhite;
+          background: #000;
+          color: #fff;
         }
         .neo-home-btn span {
           position: relative;
@@ -28,7 +28,7 @@ const HomeButton = () => {
           transition: color 0.4s;
         }
         .neo-home-btn:hover span {
-          color: black;
+          color: #000;
         }
         .neo-home-btn::before {
           content: "";
@@ -38,12 +38,12 @@ const HomeButton = () => {
           width: 120%;
           height: 100%;
           z-index: 0;
-          background: #fff;
-          transform: skew(30deg);
+          background: #6244e7;
+          transform: skew(30deg) translate3d(-100%, 0, 0);
           transition: transform 0.4s cubic-bezier(0.3, 1, 0.8, 1);
         }
         .neo-home-btn:hover::before {
-          transform: translate3d(100%, 0, 0);
+          transform: skew(30deg) translate3d(0%, 0, 0);
         }
       `}</style>
       <button className="neo-home-btn">
